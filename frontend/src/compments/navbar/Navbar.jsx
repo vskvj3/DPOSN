ccc91196ca0fd51619fe74e52e614112accaa1c6
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2024-01-28 00:35:47 +0530

    feat: add navbar placeholder

--- frontend/src/compments/navbar/Navbar.jsx
+++ frontend/src/compments/navbar/Navbar.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
+import NavbarLinks from './NavbarLinks'
+import UserLinks from './UserLinks'
 
 function Navbar() {
   return (
-    <div className="flex justify-between items-center border-b border-gray-100 w-full px-44 py-2">
-      {/* <Link to="/"> */}
+    <div className="flex justify-between items-center border-b border-gray-100 w-full md:px-44 px-5 py-2">
       <div className="text-3xl font-extrabold text-gray-900 dark:text-white font-roboto">
         <span className="text-transparent bg-clip-text bg-gradient-to-r to-red-600 from-blue-400">
           DPOSN
-        </span>{' '}
-        App
+        </span>
       </div>
-      {/* </Link> */}
-      <div className="flex justify-center item-center mx-auto">
-        {/* <NavLinks></NavLinks> */}
-        navbar links
+
+      <div className="hidden md:flex justify-center item-center ">
+        <NavbarLinks />
       </div>
-      <div>
-        {/* <UserLinks></UserLinks> */}
-        user links
+      <div className="hidden md:flex justify-center item-center ">
+        <UserLinks />
       </div>
     </div>
   )

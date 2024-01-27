13ef9d36783205700888a56279eba567033f537f
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2024-01-27 17:32:59 +0530

    boilerplate: dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Dashboard/>
     </>
   )
 }
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+function Dashboard() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">This is dashboard</h1>
+        </>
+    )
+}
+
+export default Dashboard;

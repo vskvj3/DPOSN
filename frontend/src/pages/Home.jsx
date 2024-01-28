bc495e5f4abcf4f89cf73117c94ad56f0414ea8c
Abhishek Raymond <abhishekraymond007@gmail.com>
2024-01-28 14:28:21 +0530

    Added signup page as well as basic routing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Navbar from '../compments/navbar/Navbar'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <>
-      <div>Home</div>
+      <Navbar />
+      <div>
+        <ul>
+          <li>
+            <Link to="/signup">Create account</Link>
+          </li>
+          <li>
+            <Link to="/login">Login here</Link>
+          </li>
+        </ul>
+      </div>
     </>
   )
 }
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
 import { useState, React } from 'react'
-
+import Navbar from '../compments/navbar/Navbar'
+import { Link } from 'react-router-dom'
 function LoginPage() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -25,35 +26,42 @@ function LoginPage() {
 
   return (
     <>
-      <div className="grid place-content-center w-screen h-screen bg-cyan-950">
-        <div className="bg-white w-[23rem] h-[20rem]  sm:w-[26rem] sm:h-[20rem]  flex flex-col items-center rounded-lg outline outline-offset-2 outline-1">
-          <h1 className="mt-4 text-xl">Log in</h1>
+      <div className="w-screen h-screen flex flex-col">
+        <Navbar />
+        <div className="grid place-content-center w-screen h-screen bg-cyan-950">
+          <div className="bg-white w-[23rem] h-[20rem]  sm:w-[26rem] sm:h-[20rem]  flex flex-col items-center rounded-lg outline outline-offset-2 outline-1">
+            <h1 className="mt-4 text-xl">Log in</h1>
 
-          <form className="flex flex-col w-80 h-56 mt-8">
-            <input
-              className="h-10 px-3 border-2 border-gray-500 p-2 rounded-md focus:border-teal-500 focus:outline-none"
-              placeholder="Email"
-              type="email"
-              value={email}
-              name="email"
-              onChange={handleChange}
-            ></input>
-            <input
-              className="h-10 mt-4 px-3 border-2 border-gray-500 p-2 rounded-md focus:border-teal-500 focus:outline-none"
-              placeholder="Password"
-              type="password"
-              value={password}
-              name="password"
-              onChange={handleChange}
-            ></input>
+            <form className="flex flex-col w-80 h-56 mt-8 ">
+              <input
+                className="h-10 px-3 border-2 border-gray-500 p-2 rounded-md focus:border-teal-500 focus:outline-none"
+                placeholder="Email"
+                type="email"
+                value={email}
+                name="email"
+                onChange={handleChange}
+              ></input>
+              <input
+                className="h-10 mt-4 px-3 border-2 border-gray-500 p-2 rounded-md focus:border-teal-500 focus:outline-none"
+                placeholder="Password"
+                type="password"
+                value={password}
+                name="password"
+                onChange={handleChange}
+              ></input>
 
-            <button
-              className="h-10 bg-pink-600 mt-10 rounded-lg text-white"
-              onClick={handleSubmit}
-            >
-              Log in
-            </button>
-          </form>
+              <p className="mt-5">
+                Don't have an account? <Link to="/signup">Create account</Link>
+              </p>
+
+              <button
+                className="h-10 bg-pink-600 mt-5 rounded-lg text-white"
+                onClick={handleSubmit}
+              >
+                Log in
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </>

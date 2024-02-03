77a8937f2eb9ba55ce764d8c5fa7cc8196810d3c
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2024-02-03 13:17:06 +0530

    feat: add main section to the dashboard

--- client/src/components/Dashboard/MainSection.jsx
+++ client/src/components/Dashboard/MainSection.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
+import { posts, user } from '../../assets/tempdata'
+import PostCard from './PostCard'
+
 function MainSection() {
-  return <div>MainSection</div>
+  return (
+    <div>
+      {posts?.map((post) => (
+        <PostCard
+          key={post?._id}
+          post={post}
+          user={user}
+          deletePost={() => {}}
+          likePost={() => {}}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default MainSection

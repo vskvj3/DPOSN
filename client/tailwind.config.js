e6a9a592434e637fcd0b29dba72bd61cf060f155
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2024-02-03 13:31:05 +0530

    fix: remove unwanted images

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,7 +7,19 @@ export default {
     './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgColor: 'rgb(var(--color-bg) / <alpha-value>)',
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        blue: 'rgb(var(--color-blue) / <alpha-value>)',
+        white: 'rgb(var(--color-white) / <alpha-value>)',
+        ascent: {
+          1: 'rgb(var(--color-ascent1) / <alpha-value>)',
+          2: 'rgb(var(--color-ascent2) / <alpha-value>)',
+        },
+      },
+    },
   },
   plugins: [],
 }

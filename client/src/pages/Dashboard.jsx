033317352e0771cf192e913aee665cf12b83b41b
Abin-Augustine10 <abinaugustine1010@gmail.com>
2024-02-08 11:28:27 +0530

    fix: remove scrollbar from dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -16,7 +16,7 @@ function Dashboard() {
           </div>
 
           {/* CENTER */}
-          <div className="flex-1 h-full px-4 flex flex-col gap-6 overflow-y-auto rounded-lg">
+          <div className="flex-1 h-full px-4 flex flex-col gap-6 overflow-y-auto rounded-lg no-scrollbar">
             <MainSection />
           </div>
 

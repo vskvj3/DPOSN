749898d3e983a3ef96c1941f4bc1779bf4456d45
Abin-Augustine10 <abinaugustine1010@gmail.com>
2024-02-08 11:22:05 +0530

    fix: transparent navbar issue

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import UserLinks from './UserLinks'
 
 function Navbar() {
   return (
-    <div className="flex justify-between items-center border-b border-gray-100 w-full  md:px-44 px-5 py-2">
+    <div className="flex justify-between items-center border-b border-gray-100 w-full  md:px-44 px-5 py-2 bg-white">
       <div className="text-3xl text-gray-900 dark:text-white font-roboto">
         <span className="text-black">DPOSN</span>
       </div>

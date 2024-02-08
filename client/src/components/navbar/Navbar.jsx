fc4b31b4facb495d622c66640d98c9e9b0838147
Abin-Augustine10 <abinaugustine1010@gmail.com>
2024-02-08 11:15:55 +0530

    fix: linting issues

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -6,9 +6,7 @@ function Navbar() {
   return (
     <div className="flex justify-between items-center border-b border-gray-100 w-full  md:px-44 px-5 py-2">
       <div className="text-3xl text-gray-900 dark:text-white font-roboto">
-        <span className="text-black">
-          DPOSN
-        </span>
+        <span className="text-black">DPOSN</span>
       </div>
 
       <div className="hidden md:flex justify-center item-center ">

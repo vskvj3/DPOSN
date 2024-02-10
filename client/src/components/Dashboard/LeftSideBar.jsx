dc5aaa0fa2a9edaf6e68b307c6b375ac5f14c18c
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2024-02-10 16:37:49 +0530

    fix: position of profile card

--- client/src/components/Dashboard/LeftSideBar.jsx
+++ client/src/components/Dashboard/LeftSideBar.jsx
@@ -3,7 +3,7 @@ import ProfileCard from './ProfileCard'
 
 function LeftSideBar() {
   return (
-    <div className="w-full pl-6 py-5">
+    <div className="w-full pl-6">
       <ProfileCard />
     </div>
   )

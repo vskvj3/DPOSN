6be649c07fbe8d643a350ff499e3a719f46dccfa
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2024-02-15 14:43:02 +0530

    fix: document nft upload

--- client/src/ipfs-utils/IpfsUtils.jsx
+++ client/src/ipfs-utils/IpfsUtils.jsx
@@ -5,6 +5,11 @@ dotenv.config()
 
 const NFT_STORAGE_KEY = process.env.NFT_STORAGE_KEY
 
+/**
+ * Upload file to IPFS
+ * @param {File} file file to upload in File blob format
+ * @returns {String} CID of the file uploaded to IPFS
+ */
 async function uploadFileToIPFS(file) {
   try {
     const nftstorage = new NFTStorage({ token: NFT_STORAGE_KEY })
@@ -16,6 +21,11 @@ async function uploadFileToIPFS(file) {
   }
 }
 
+/**
+ * upload text to IPFS
+ * @param {String} text text to upload to IPFS in string format
+ * @returns {String} CID of the text uploaded to IPFS
+ */
 async function uploadTextToIPFS(text) {
   try {
     const textBlob = new File([text], 'response.json', { type: 'text/plain' })

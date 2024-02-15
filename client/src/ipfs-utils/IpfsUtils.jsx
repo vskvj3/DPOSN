9d0b0adebad55df6ad4821e38c9c54f1b9706697
Visakh Vijay <92223768+vskvj3@users.noreply.github.com>
2024-02-15 16:51:43 +0530

    feat: upload image files and json files to Ipfs using ntf.storage API (#25)
    * feat: upload to nft.storage
    
    * fix: document nft upload
    
    * fix: document nft upload
    
    * fix: document nft upload

---
+++ client/src/ipfs-utils/IpfsUtils.jsx
@@ -0,0 +1,43 @@
+// Code to upload files and text to IPFS using NFT.Storage
+
+import dotenv from 'dotenv'
+import { NFTStorage } from 'nft.storage'
+
+dotenv.config()
+
+const NFT_STORAGE_KEY = process.env.NFT_STORAGE_KEY
+
+/**
+ * Upload file to IPFS
+ * @param {File} file file to upload in File blob format
+ * @returns {String} CID of the file uploaded to IPFS
+ */
+async function uploadFileToIPFS(file) {
+  try {
+    const nftstorage = new NFTStorage({ token: NFT_STORAGE_KEY })
+    const cid = await nftstorage.storeBlob(file)
+    console.log(cid)
+    return cid
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+/**
+ * upload text to IPFS
+ * @param {String} text text to upload to IPFS as json string
+ * @returns {String} CID of the text uploaded to IPFS
+ */
+async function uploadTextToIPFS(text) {
+  try {
+    const textBlob = new File([text], 'response.json', { type: 'text/plain' })
+    const nftstorage = new NFTStorage({ token: NFT_STORAGE_KEY })
+    const cid = await nftstorage.storeBlob(textBlob)
+    console.log(cid)
+    return cid
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export { uploadFileToIPFS, uploadTextToIPFS }

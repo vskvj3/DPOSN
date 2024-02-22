4f7a401d8afe4282af9f5b5dfe18654718c5f196
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2024-02-22 17:53:20 +0530

    fix: show connected to blockchain network or not

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import UserLinks from './UserLinks'
-import Hamburger from './Hamburger'
-import './index.css'
+import EthContext from '../../contexts/EthContext'
+import { FaEthereum } from 'react-icons/fa'
 
 function Navbar() {
+  const {
+    state: { contract, accounts, web3, networkID },
+  } = useContext(EthContext)
+
   return (
     // move the userlinks to left most position
     <div className="flex justify-between items-center border-b border-gray-100 w-full lg:rounded-b-md  md:px-44 px-5 py-2 bg-white">
-
       <div className="text-3xl text-gray-900 dark:text-white font-roboto">
         <span className="text-black">DPOSN</span>
       </div>
-      <div className="hidden md:flex justify-center item-center ">
+
+      <div className="flex justify-center item-center ">
+        <div className=" mr-5">
+          {networkID ? (
+            <div className="bg-slate-200 rounded-md p-3">
+              <FaEthereum color="green" />
+            </div>
+          ) : (
+            <div className="bg-slate-200 rounded-md p-3">
+              <FaEthereum className=" text-red-700" />
+            </div>
+          )}
+        </div>
         <UserLinks />
-      </div>
-      <div className="flex md:hidden justify-center item-center">
-        <Hamburger />
       </div>
     </div>
   )

08745f8bd40cdf682b69e84f014f4fa2fe08ae51
Abhishek Raymond <abhishekraymond007@gmail.com>
2024-03-23 16:14:57 +0530

    feat : added commenting functionalities

--- client/src/components/Dashboard/CommentSection.jsx
+++ client/src/components/Dashboard/CommentSection.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import NoProfile from '../../assets/images/userprofile.png'
 
-function CommentSection(comment) {
+const PINATA_GATEWAY = import.meta.env.VITE_PINATA_PRIVATE_GATEWAY_URL
+
+function CommentSection({ comment }) {
   return (
-    <div>
-      <div key={comment?._id} className="flex gap-2 items-center mt-2">
+    <div className="mt">
+      <div className="flex gap-2 items-center mt-2">
         <img
-          src={comment?.userId?.profileUrl ?? NoProfile}
-          alt={comment?.userId?.firstName}
+          src={
+            comment.profileUrl
+              ? `${PINATA_GATEWAY}/ipfs/${comment.profileUrl}`
+              : NoProfile
+          }
+          alt={comment.userName}
           className="w-10 h-10 object-cover rounded-full"
         />
         <div>
           <p className="text-base font-medium text-ascent-1">
-            {comment?.userId?.firstName} {comment?.userId?.lastName}
+            {comment.userName}
           </p>
-          <span className="text-sm text-ascent-2">{comment?.comment}</span>
+          <span className="text-sm text-ascent-2">{comment.comment}</span>
         </div>
       </div>
     </div>
--- client/src/pages/Pages.jsx
+++ client/src/pages/Pages.jsx
@@ -6,6 +6,7 @@ import Cookies from 'js-cookie'
 function Pages() {
   let logedIn = Cookies.get('loggedIn')
   console.log('logged in cookie: ' + logedIn)
+
   return <>{logedIn ? <Dashboard /> : <Home />}</>
 }
 
